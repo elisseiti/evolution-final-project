b1868585c84158a3ddc82228dc48a5733dd44c0b
Elis Seiti <elisseiti95@gmail.com>
2023-04-12 19:14:35 +0200

    Add DoobieOrderRepository

--- build.sbt
+++ build.sbt
@@ -7,11 +7,17 @@ scalaVersion := "2.13.10"
 lazy val doobieVersion = "1.0.0-RC1"
 lazy val catsVersion = "3.4.8"
 lazy val doobiePostgres = "1.0.0-RC1"
+lazy val http4sVersion = "1.0.0-M39"
+
 
 libraryDependencies ++= Seq(
   "org.tpolecat" %% "doobie-core"     % doobieVersion,
   "org.tpolecat" %% "doobie-postgres" % doobieVersion,
   "org.tpolecat" %% "doobie-specs2"   % doobieVersion,
   "org.typelevel" %% "cats-effect" % catsVersion,
-  "org.tpolecat" %% "doobie-postgres" % doobiePostgres
+  "org.tpolecat" %% "doobie-postgres" % doobiePostgres,
+  "org.http4s" %% "http4s-ember-client" % http4sVersion,
+  "org.http4s" %% "http4s-ember-server" % http4sVersion,
+  "org.http4s" %% "http4s-dsl"          % http4sVersion
 )
+
--- src/main/scala/repository/doobie/DoobieOrderRepository.scala
+++ src/main/scala/repository/doobie/DoobieOrderRepository.scala
@@ -1,49 +1,51 @@
-//package repository.doobie
-//
-//import algebras.OrderRepositoryAlgebra
-//import cats.effect.Sync
-//import cats.implicits.catsSyntaxOptionId
-//import domain.entity.Orders
-//import doobie._
-//import doobie.implicits._
-//
-//private object OrderSQL {
-//  /* We require type StatusMeta to handle our ADT Status */
-//
-//  def select(orderId: Long): Query0[Orders] = sql"""
-//    SELECT ID, USER_ID, RESTAURANT_ID, MEAL_IDS, TOTAL_PRICE
-//    FROM ORDERS
-//    WHERE ID = $orderId
-//  """.query[Orders]
-//
-//  def insert(order: Orders): Update0 = sql"""
-//    INSERT INTO ORDERS (USER_ID, RESTAURANT_ID, MEAL_IDS, TOTAL_PRICE)
-//    VALUES (${order.userId}, ${order.restaurantId}, ${order.mealIds}, ${order.totalPrice})
-//  """.update
-//
-//  def delete(orderId: Long): Update0 = sql"""
-//    DELETE FROM ORDERS
-//    WHERE ID = $orderId
-//  """.update
-//}
-//
-//class DoobieOrderRepository[F[_]: Sync](val xa: Transactor[F])
-//    extends OrderRepositoryAlgebra[F] {
-//
-//  override def create(order: Orders): F[Orders] = OrderSQL
-//    .insert(order)
-//    .withUniqueGeneratedKeys[Long]("ID")
-//    .map(id => order.copy(id = id.some))
-//    .transact(xa);
-//
-//  override def get(orderId: Long): F[Option[Orders]] = OrderSQL.select(orderId).option.transact(xa)
-//
-//
-//}
-//
-//object DoobieOrderRepository {
-//  def apply[F[_]: Sync](
-//                         xa: Transactor[F],
-//                       ): DoobieOrderRepository[F] =
-//    new DoobieOrderRepository(xa)
-//}
+package repository.doobie
+
+import algebras.OrderRepositoryAlgebra
+import cats.effect.Sync
+import cats.implicits.catsSyntaxOptionId
+import domain.entity.Orders
+import doobie._
+import doobie.implicits._
+
+private object OrderSQL {
+  /* We require type StatusMeta to handle our ADT Status */
+
+  // Meta[String].timap(fromString-toSet[Long])(fromSet[Long]toString)
+  implicit val SetLongMeta: Meta[Set[Long]] = Meta[String].imap(_.split(',').toList.map(el => el.toLong).toSet)(_.mkString(","))
+
+  def select(orderId: Long): Query0[Orders] = sql"""
+    SELECT ID, USER_ID, RESTAURANT_ID, MEAL_IDS, TOTAL_PRICE
+    FROM ORDERS
+    WHERE ID = $orderId
+  """.query[Orders]
+
+  def insert(order: Orders): Update0 = sql"""
+    INSERT INTO ORDERS (USER_ID, RESTAURANT_ID, MEAL_IDS, TOTAL_PRICE)
+    VALUES (${order.userId}, ${order.restaurantId}, ${order.mealIds}, ${order.totalPrice})
+  """.update
+
+  def delete(orderId: Long): Update0 = sql"""
+    DELETE FROM ORDERS
+    WHERE ID = $orderId
+  """.update
+}
+
+class DoobieOrderRepository[F[_]: Sync](val xa: Transactor[F])
+    extends OrderRepositoryAlgebra[F] {
+
+  override def create(order: Orders): F[Orders] = OrderSQL
+    .insert(order)
+    .withUniqueGeneratedKeys[Long]("ID")
+    .map(id => order.copy(id = id.some))
+    .transact(xa);
+
+  override def get(orderId: Long): F[Option[Orders]] = OrderSQL.select(orderId).option.transact(xa)
+
+}
+
+object DoobieOrderRepository {
+  def apply[F[_]: Sync](
+                         xa: Transactor[F],
+                       ): DoobieOrderRepository[F] =
+    new DoobieOrderRepository(xa)
+}
