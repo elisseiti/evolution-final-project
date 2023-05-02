8acc7752e4566ae876fdad06d2f186b3237be863
Elis Seiti <elisseiti95@gmail.com>
2023-05-02 21:54:42 +0200

    Update Restaurant service structure

--- src/main/scala/service/RestaurantService.scala
+++ src/main/scala/service/RestaurantService.scala
@@ -1,15 +1,20 @@
 package service
 
-import cats.effect.IO
+import algebras.RestaurantRepositoryAlgebra
+import algebras.validation.RestaurantValidationAlgebra
+import cats.Monad
 import entity.{RegularUser, Restaurant}
 
-trait RestaurantServiceTrait {
-  def create(restaurant: Restaurant): IO[Either[Error, Restaurant]]
-  def get(username: String): IO[Option[RegularUser]]
+trait RestaurantServiceTrait[F[_]] {
+  def create(restaurant: Restaurant): F[Either[Error, Restaurant]]
+  def get(username: String): F[Option[RegularUser]]
 }
 
-class RestaurantService extends RestaurantServiceTrait{
-  override def create(user: Restaurant): IO[Either[Error, Restaurant]] = ???
+class RestaurantService[F[_]: Monad](restaurantRepository: RestaurantRepositoryAlgebra[F],
+                                      restaurantValidationAlgebra: RestaurantValidationAlgebra[F]
+                                     )
+  extends RestaurantServiceTrait[F]{
+  override def create(user: Restaurant): F[Either[Error, Restaurant]] = ???
 
-  override def get(username: String): IO[Option[RegularUser]] = ???
+  override def get(username: String): F[Option[RegularUser]] = ???
 }
